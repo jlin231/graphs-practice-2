e818f155a2fa4fa81f607f6916767c9017e7f2f1
jlin231 <jonlin981@gmail.com>
2022-11-03 15:45:58 -0700

    countIslands

--- island.js
+++ island.js
@@ -1,37 +1,84 @@
-function getNeighbors(row, col, matrix) {
+function getNeighbors(row, col, graph) {
   
+  let coordinates = []
   // Check top
-  // Check top right
-  // Check right
-  // Check bottom right
-  // Check bottom
-  // Check bottom left
-  // Check left
-  // Check top left
-  // Return neighbors
+  if (row !== 0 && graph[row-1][col] === 1){
+    coordinates.push([row-1, col])
+  }
   
-  // Your code here
+  // Check bottom
+  if (row < graph.length-1 && graph[row+1][col] === 1){
+    coordinates.push([row+1, col])
+  }
+  // Check left
+  if (col !== 0 && graph[row][col-1] === 1){
+    coordinates.push([row, col-1])
+  }
+  // Check right
+  if (col < graph[0].length - 1 && graph[row][col + 1] === 1){
+    coordinates.push([row, col +1])
+  }
+  //top left
+  if(row !== 0 && col !== 0 && graph[row-1][col-1]=== 1){
+    coordinates.push([row-1,col-1])
+  }
+  //top right
+  if(col < graph[0].length-1 && row !== 0 && graph[row-1][col+1]=== 1){
+    coordinates.push([row-1,col+1])
+  }
+  //bottom left
+  if(row< graph.length-1 && col !== 0 && graph[row+1][col-1]=== 1){
+    coordinates.push([row+1, col-1])
+  }
+  //bottom right
+  if(row < graph.length-1 && col < graph[0].length - 1 && graph[row+1][col+1]=== 1){
+    coordinates.push([row+1, col+1]);
+  }
+  // Return neighbors
+  console.log(coordinates)
+  return coordinates
 }
 
 function countIslands(matrix) {
-  
   // Create a visited set to store visited nodes
+  let visited = new Set();
   // Initialize count to 0
+  let count = 0;
   // Iterate through all indices in matrix
-    // If an index contains a 1 and has not been visited, 
-    // increment island count and start traversing neighbors
-      // DO THE THING (increment island count by 1)
-      // Initialize a stack with current index
-      // Add stringified version of current index to the visited set
-      // While stack contains elements
-        // Pop element from stack
-        // Get valid neighbors of current element
-        // Iterate over neigbors
-          // If neighbor has not been visited
-            // Add neighbor to stack
-            // Mark neighbor as visited
+  for(let row = 0; row < matrix.length; row++){
+    for(let col = 0; col < matrix[0].length; col++){
+      // If an index contains a 1 and has not been visited, 
+      let currentNode = [row, col]; 
+      if(!visited.has(currentNode.toString()) && matrix[row][col] === 1){
+        // increment island count and start traversing neighbors
+        // DO THE THING (increment island count by 1)
+        count++;
+        // Initialize a stack with current index
+        let stack = [currentNode];
+        // Add stringified version of current index to the visited set
+        visited.add([row,col].toString())
+        // While stack contains elements
+        while(stack.length){
+          // Pop element from stack
+          let node = stack.pop()
+          // Get valid neighbors of current element
+          let neighbors = getNeighbors(node[0], node[1], matrix);
+          // Iterate over neigbors
+          neighbors.forEach((ele) =>{
+            // If neighbor has not been visited
+            if(!visited.has(ele.toString())){
+              // Mark neighbor as visited
+              visited.add(ele.toString());
+              // Add neighbor to stack
+              stack.push(ele);;
+            }
+          })
+        }
+      }
+    }
+  }
   // Return island count
-  
+  return count;
   // Your code here
 }
 
